45fa331173c01e49234d894dd66a5763963f750d
felipe_l_rodrigues <felipe_l_rodrigues@SN-371716>
2025-06-12 20:26:37 -0300

    Adicionar arquivos de projeto.

---
+++ Exec02/Program.cs
@@ -0,0 +1,23 @@
+﻿enum State { On, off }
+class Lamp(State State)
+{
+    public void Operate()
+    {
+        State = State == State.On ? State.off : State.On;
+        Console.WriteLine($"The lamp is now {State}.");
+
+    }
+}
+
+class Switch
+{
+    private Lamp lamp;
+    public Switch(Lamp lamp)
+    {
+        this.lamp = lamp;
+    }
+    public void Press()
+    {
+        this.lamp.Operate();
+    }
+}
